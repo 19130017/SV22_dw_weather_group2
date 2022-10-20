cbeadfad24004b578726f7f285a5a80a17f1c334
19130017 <93698875+19130017@users.noreply.github.com>
2022-10-20 12:59:19 +0700

    initial project

---
+++ data_warehouse_weather/src/main/java/vn/dtbinh1511/data_warehouse_weather/HelloApplication.java
@@ -0,0 +1,9 @@
+package vn.dtbinh1511.data_warehouse_weather;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
